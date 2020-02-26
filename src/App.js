f6ac7eb2e5c80aec1833e4f7352ac7e0f03661bf
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2020-02-26 11:45:56 -0300

    Correct sizes on div wrapper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import logo from './assets/logo.png';
-import logosvg from './assets/facebook.svg';
-
 import Head from './components/Head/Head';
 import PostList from './components/PostList/PostList';
 
